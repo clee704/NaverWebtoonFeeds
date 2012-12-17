07daf138122a1cbfe91bd02b2a0484fd0c8a7daf
Choongmin Lee <choongmin@me.com>
2012-12-18 05:57:30 +0900

    Remove now unnecessary gunicornconfig.py

--- gunicornconfig.py
+++
@@ -1,9 +0,0 @@
-import multiprocessing
-
-bind = 'unix:/tmp/naverwebtoonfeeds.sock'
-workers = multiprocessing.cpu_count() * 2 + 1
-worker_class = 'gevent'
-user = 'www-data'
-group = 'nogroup'
-umask = '0007'
-log_file = '/tmp/gunicorn_errors.log'
