2f5c3085b3d88a6085acbeb85fdc87379380f3be
Choongmin Lee <choongmin@me.com>
2012-12-23 18:29:22 +0900

    Use Redis instead of Memcached

--- naverwebtoonfeeds/heroku_settings.py
+++ naverwebtoonfeeds/heroku_settings.py
@@ -1,18 +1,14 @@
-import logging, os
-URL_ROOT = os.environ['URL_ROOT']
+import logging
+import os
+import re
+
 LOG_LEVEL = getattr(logging, os.environ['LOG_LEVEL'])
-SEND_EMAIL = os.environ['SEND_EMAIL'] == '1'
 EMAIL_LEVEL = getattr(logging, os.environ['EMAIL_LEVEL'])
 MAIL_HOST = ('smtp.gmail.com', 587)
-MAIL_FROMADDR = os.environ['FROMADDR']
-MAIL_TOADDRS = [os.environ['ADMIN_EMAIL']]
+MAIL_TOADDRS = re.split('\s*;\s*', os.environ['MAIL_TOADDRS'])
 MAIL_CREDENTIALS = (os.environ['GMAIL_USERNAME'], os.environ['GMAIL_PASSWORD'])
 MAIL_SECURE = ()
-SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-NAVER_USERNAME = os.environ['NAVER_USERNAME']
-NAVER_PASSWORD = os.environ['NAVER_PASSWORD']
-CACHE_TYPE = 'memcached'
-CACHE_KEY_PREFIX = 'naverwebtoonfeeds'
-CACHE_MEMCACHED_SERVERS = [os.environ['MEMCACHE_SERVERS']]
-CACHE_MEMCACHED_USERNAME = os.environ['MEMCACHE_USERNAME']
-CACHE_MEMCACHED_PASSWORD = os.environ['MEMCACHE_PASSWORD']
+
+for key, value in os.environ.items():
+  if key not in locals():
+    locals()[key] = value
