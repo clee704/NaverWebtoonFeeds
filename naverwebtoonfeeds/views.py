57514c7b36ed610e43f8c43a396e8f6895dc9b6c
Choongmin Lee <choongmin@me.com>
2013-01-13 03:38:25 +0900

    Fix bug where index view cache is not purged

--- naverwebtoonfeeds/views.py
+++ naverwebtoonfeeds/views.py
@@ -45,7 +45,9 @@ def feed_show(series_id):
     app.logger.info('feed_show, series_id=%d (GET %s)', series_id, url)
     # update_series_list with no argument only adds new series.
     # The current series never gets updated.
-    update_series_list()
+    list_updated, _ = update_series_list()
+    if list_updated:
+        cache.delete('feed_index')
     series = Series.query.get_or_404(series_id)
     updated = False
     if series.new_chapters_available:
