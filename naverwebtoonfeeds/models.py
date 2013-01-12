f7ee4747a98ee365ab04f9a09d042c05fa123498
Choongmin Lee <choongmin@me.com>
2013-01-13 01:26:54 +0900

    Change DB column types from Text to String

--- naverwebtoonfeeds/models.py
+++ naverwebtoonfeeds/models.py
@@ -6,23 +6,23 @@ db = SQLAlchemy()
 
 class Series(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.Text, nullable=False, index=True)
-    author = db.Column(db.Text, nullable=False)
+    title = db.Column(db.String(255), nullable=False, index=True)
+    author = db.Column(db.String(255), nullable=False)
     description = db.Column(db.Text)
     last_chapter = db.Column(db.Integer, nullable=False)
     is_completed = db.Column(db.Boolean, nullable=False, default=False, index=True)
-    thumbnail_url = db.Column(db.Text)
-    update_days = db.Column(db.Text)
+    thumbnail_url = db.Column(db.String(255))
+    update_days = db.Column(db.String(31))
 
     new_chapters_available = db.Column(db.Boolean, default=False)
-    last_update_status = db.Column(db.Text, nullable=False, default='')
+    last_update_status = db.Column(db.String(31), nullable=False, default='')
 
 
 class Chapter(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.Text, nullable=False)
+    title = db.Column(db.String(255), nullable=False)
     pubdate = db.Column(db.DateTime, nullable=False)
-    thumbnail_url = db.Column(db.Text)
+    thumbnail_url = db.Column(db.String(255))
     series_id = db.Column(db.Integer, db.ForeignKey('series.id'), primary_key=True)
     series = db.relationship('Series', backref=db.backref('chapters', order_by=id.desc()))
 
