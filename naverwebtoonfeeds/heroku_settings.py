b1c57e2ef63ff8f38ff3dc3630a65463db1ca7ac
Choongmin Lee <choongmin@me.com>
2013-01-12 16:25:06 +0900

    Guess the type of environment variables

--- naverwebtoonfeeds/heroku_settings.py
+++ naverwebtoonfeeds/heroku_settings.py
@@ -2,28 +2,30 @@ import logging
 import os
 import re
 
-if os.environ.get('LOG_LEVEL'):
-    LOG_LEVEL = getattr(logging, os.environ['LOG_LEVEL'].upper())
-
-if os.environ.get('EMAIL_LEVEL'):
-    EMAIL_LEVEL = getattr(logging, os.environ['EMAIL_LEVEL'].upper())
-
-MAIL_HOST = ('smtp.gmail.com', 587)
-
-if os.environ.get('MAIL_TOADDRS'):
-    MAIL_TOADDRS = re.split(r'\s*;\s*', os.environ['MAIL_TOADDRS'])
-
 if os.environ.get('GMAIL_USERNAME') and os.environ.get('GMAIL_PASSWORD'):
     MAIL_CREDENTIALS = (os.environ['GMAIL_USERNAME'], os.environ['GMAIL_PASSWORD'])
 
+MAIL_HOST = ('smtp.gmail.com', 587)
 MAIL_SECURE = ()
 
-if os.environ.get('SEND_FILE_MAX_AGE_DEFAULT'):
-    SEND_FILE_MAX_AGE_DEFAULT = int(os.environ['SEND_FILE_MAX_AGE_DEFAULT'])
-
-if os.environ.get('CACHE_MEMCACHED_SERVERS'):
-    CACHE_MEMCACHED_SERVERS = re.split(r'\s*,\s*', os.environ['CACHE_MEMCACHED_SERVERS'])
+def guess_type(value):
+    if re.match(r'^\d+$', value):
+        return int(value)
+    elif re.match(r'^\d+\.\d+$', value):
+        return float(value)
+    elif value == 'True':
+        return True
+    elif value == 'False':
+        return False
+    elif re.match(r'^\[.*\]$', value):
+        return re.split(r'\s*,\s*', value[1:-1])
+    else:
+        return value
 
 for key, value in os.environ.items():
     if key not in locals():
-        locals()[key] = value
+        if key.endswith('_LEVEL'):
+            val = getattr(logging, value.upper())
+        else:
+            val = guess_type(value)
+        locals()[key] = val
