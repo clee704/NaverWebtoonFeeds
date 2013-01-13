0bc1529bb741a697299c36989fb01d909c200b4c
Choongmin Lee <choongmin@me.com>
2013-01-14 01:10:05 +0900

    Log the current IP at the start up

--- app.py
+++ app.py
@@ -20,10 +20,16 @@ if app.config.get('SEND_EMAIL'):
     for logger in loggers:
         logger.addHandler(smtp_handler)
 
+try:
+    from naverwebtoonfeeds.lib.naver import get_public_ip
+    app.logger.warning('Current IP: %s', get_public_ip())
+except:
+    pass
+
 if __name__ == '__main__':
     try:
         import os
         port = int(os.environ.get('PORT', 5000))
         app.run(host='0.0.0.0', port=port)
-    except Exception:
+    except:
         app.logger.critical('A critical error occurred', exc_info=True)
