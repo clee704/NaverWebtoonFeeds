6a2a7133e3a173f8fcd0174c7e7a77adaf798d32
Choongmin Lee <choongmin@me.com>
2013-01-26 23:46:56 +0900

    Make mock_obj return a Mock object without a spec

--- naverwebtoonfeeds/test/utilities.py
+++ naverwebtoonfeeds/test/utilities.py
@@ -1,9 +1,8 @@
-from flask import Flask
 from mock import Mock
 
 
 def mock_obj(**kwargs):
-    mock = Mock(Flask('mock'))
+    mock = Mock()
     for key, value in kwargs.items():
         setattr(mock, key, value)
     return mock
