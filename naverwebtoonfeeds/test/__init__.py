55ea0ba6c57dbf9e43236e61b6a97beca182cfca
Choongmin Lee <choongmin@me.com>
2013-01-27 02:43:16 +0900

    Update the name of the executable

--- naverwebtoonfeeds/test/__init__.py
+++ naverwebtoonfeeds/test/__init__.py
@@ -3,5 +3,5 @@ import os
 def setup_package():
     mode = os.environ.get('NWF_MODE', '')
     if mode != 'test':
-        print "Run tests with 'runtest', not 'nosetests'."
+        print "Run tests with 'runtests', not 'nosetests'."
         raise RuntimeError("Invalid NWF_MODE: '{0}'. It must be 'test'.".format(mode))
